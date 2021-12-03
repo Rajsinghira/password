302a279b7835772dda2101a5baab367cc0bbbbe4
root <root@localhost>
2021-12-03 21:48:19 +0530

    auto publish 1638548298802

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from random import *
 user_pass=input("enter your name")
 password=['a','b','c','d','e','f','g','h','i','j','k']
 guess=""
+while(guess!=user_pass):
+  guess=""
+
+  for letter in range(len(user_pass)):
+  
